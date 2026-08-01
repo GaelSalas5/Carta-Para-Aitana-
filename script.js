9db5e6c58b4587cc9734689637c1a23633fea0a6
GaelSalas5 <97ddfrhwdh@privaterelay.appleid.com>
2026-08-01 14:22:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-function abrirCarta(){if(document.getElementById('password').value==='24Noviembre'){login.style.display='none';carta.style.display='block';}else{error.textContent='Contraseña incorrecta';}}
+function abrirCarta(){
+  if(document.getElementById('password').value==='24Noviembre'){
+    
+    login.style.opacity='0';
+    
+    setTimeout(()=>{
+      login.style.display='none';
+      carta.style.display='block';
+      carta.style.opacity='0';
+      carta.style.transform='scale(.8)';
+
+      setTimeout(()=>{
+        carta.style.opacity='1';
+        carta.style.transform='scale(1)';
+      },100);
+
+    },500);
+
+  }else{
+    error.textContent='Contraseña incorrecta';
+  }
+}
